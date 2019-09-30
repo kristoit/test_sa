3992a892f6dec28ec8849f9213ea523c65d81a0c
kristoit <tozter@gmail.com>
2019-10-01 01:04:16 +0300

    add files 2

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "11.Docker.Lading test 2"
+    return "11.Docker.Lading test 3"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("8080"), debug=True)
