0a057393bc0565145cc8094fbb860473ab53671c
kristoit <tozter@gmail.com>
2019-10-01 01:00:30 +0300

    del directory

--- Docker_test/test.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "11.Docker.Lading test 2"
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("8080"), debug=True)
